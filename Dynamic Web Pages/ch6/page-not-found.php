dbe973601b313b3e2058696961431ba8ca2bed1b
danieldumitru77 <danieldumitru77@yahoo.com>
2023-11-30 19:38:00 +0100

    ch6 first

---
+++ Dynamic Web Pages/ch6/page-not-found.php
@@ -0,0 +1,5 @@
+<?php include 'includes/header.php' ?>
+
+<h1> Page Not FOUND</h1> 
+<p>Sorry , find</p>
+<?php include 'includes/footer.php' ?>
---
+++ Dynamic Web Pages/ch6/xss-2.php
@@ -0,0 +1,11 @@
+<a class="badlink" href="xss-2.php?msg=<script src=js/>bad.js ></script>">ESCAPING MARKUP</a>
+
+<?php
+$message = $_GET['msg'] ?? 'Click link at top of page';
+?>
+<?php include 'includes/header.php' ?>
+
+<h1>XSS Example</h1>
+<p><?= htmlspecialchars($message) ?></p>
+
+<?php include 'includes/footer.php' ?>
